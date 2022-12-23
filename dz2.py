725273729d71d23ea70531f2732c86ec9b1a933e
Vlad1992PA <106955857+Vlad1992PA@users.noreply.github.com>
2022-12-23 10:43:42 +0300

    Update dz2.py

--- dz2.py
+++ dz2.py
@@ -32,8 +32,27 @@ for i in range(n):
         i = i+1
         list.append(round(((1+1/i)**i), 2))
         sum = sum + float((1+1/i)**i)
-
-
+    
 print(f'Для N числа = {n} список:', end=' ')
 print(list, sep=', ')
 print(f'Сума чисел N = {round(sum, 2)}')
+
+
+    
+        #                        Задача№3
+#Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, максимум использование библиотеки Random для и получения случайного int
+
+list = [1, 5, 8 , 9 ,10 ]
+list2 = []
+count = 0
+import random
+for i in list:
+
+    list2.append(i) 
+    num = random.randint(0, (int(len(list)) -1))
+    list[count] = list[num]
+    list[num] = i
+    count= count+1
+  
+print(f'Перемешанный список:', end=' ')
+print(list, sep=', ')
